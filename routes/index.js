0a7fac1500e4a6e9ae88be2b5573b3d9ac5d5c64
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-01-03 15:06:16 -0200

    Used i18n on routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
+var i18n = require("i18n");
+
 exports.index = function(req, res){
+  console.log(req.query.locale || "en")
+  res.cookie('locale', req.query.locale || i18n.configure.defaultLocale);
   joinchannel = req.query.channel || config.channel;
   if(joinchannel[0] != "#"){
     joinchannel = "#" + joinchannel;
   }
   res.render('index', {
     title: joinchannel + ' ichat.io',
-    joinchannel: joinchannel
+    joinchannel: joinchannel,
+    __: i18n.__
   });
 };
